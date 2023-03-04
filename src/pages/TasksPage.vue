5e0660dfa8c718ed0f1d1de151f8ee48d96287a4
Eding Muhamad <masaruedo@yahoo.com>
2023-03-04 17:55:23 +0700

    Define getters

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -43,7 +43,7 @@ import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
 
 const store = useTaskStore()
-const { task } = storeToRefs(store)
+const { completedTasks, uncompletedTasks } = storeToRefs(store)
 // store.$patch({
 //     task: {
 //         name: "First task updated using $patch",
@@ -56,11 +56,8 @@ const tasks = ref([])
 onMounted(async () => {
     const { data } = await allTasks()
     tasks.value = data.data
-    console.log(task.value)
 })
 
-const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
-const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
 const showToggleCompletedBtn = computed(
     () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
 )
