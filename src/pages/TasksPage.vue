b2bf1f9a7cfbb0e7804d29b776c37020359c80a2
Eding Muhamad <masaruedo@yahoo.com>
2023-03-04 14:45:03 +0700

    Define store

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -36,15 +36,27 @@
 </template>
 <script setup>
 import { computed, onMounted, ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useTaskStore } from "../stores/task";
 import { allTasks, createTask, updateTask, completeTask, removeTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
+
+const store = useTaskStore()
+const { task } = storeToRefs(store)
+// store.$patch({
+//     task: {
+//         name: "First task updated using $patch",
+//         is_completed: true
+//     }
+// })
 
 const tasks = ref([])
 
 onMounted(async () => {
     const { data } = await allTasks()
     tasks.value = data.data
+    console.log(task.value)
 })
 
 const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
---
+++ src/stores/task.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export const useTaskStore = defineStore("taskStore", {
+  state: () => ({
+    tasks: [],
+    task: {
+      id: 1,
+      name: "First task",
+      is_completed: false,
+    },
+  }),
+});
