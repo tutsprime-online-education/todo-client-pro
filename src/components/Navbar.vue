cfa13d780c6d2332ace4d8fc7447efbf62e7565f
Eding Muhamad <masaruedo@yahoo.com>
2023-03-11 14:43:54 +0700

    Router link active class

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,7 @@
                         <!-- <a href="#" @click.prevent="$router.push('/tasks')" class="nav-link">Tasks</a> -->
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Summary</a>
+                        <router-link :to="{ name: 'summary' }" class="nav-link">Summary</router-link>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
@@ -34,3 +34,9 @@
         </div>
     </nav>
 </template>
+
+<style scoped>
+.nav-link.router-link-active {
+    color: rgba(0,0,0,.9);
+}
+</style>
