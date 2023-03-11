df44d20270736592fe94081bc15a7cce7e13d4ff
Eding Muhamad <masaruedo@yahoo.com>
2023-03-11 14:17:23 +0700

    Setup vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import route from "./router";
 import App from "./App.vue";
 
 import "./assets/main.css";
 
 const app = createApp(App);
 app.use(createPinia());
+app.use(route);
 app.mount("#app");
