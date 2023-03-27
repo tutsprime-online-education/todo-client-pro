7c0415e21e27918b43e5cfce0a0f1e37b56ec7c5
Eding Muhamad <masaruedo@yahoo.com>
2023-03-27 09:40:00 +0700

    Fix the completed tasks issue

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -46,5 +46,5 @@ const showToggleCompletedBtn = computed(
 const completedTasksIsVisible = computed(
     () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
 )
-const showCompletedTasks = ref(false)
+const showCompletedTasks = ref(false || completedTasksIsVisible.value)
 </script>
