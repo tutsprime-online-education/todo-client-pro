9693eefecc2f75e5d4ef257fa4d304f6c650cd22
Eding Muhamad <masaruedo@yahoo.com>
2023-06-01 20:54:29 +0700

    Handle click outside in Dropdown component

--- src/components/dropdown/Dropdown.vue
+++ src/components/dropdown/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <component :is="tag" class="dropdown">
+    <component :is="tag" class="dropdown" v-click-outside="handleClickOutside">
         <slot name="toggle" :toggleClass="toggleClass" :toggle="toggle" />
         <ul class="dropdown-menu" :class="toggleClass" :style="menuStyle">
             <slot name="menu" :toggle="toggle" />
@@ -9,6 +9,9 @@
 
 <script setup>
 import { ref, computed } from "vue";
+import clickOutside from 'click-outside-vue3'
+
+const vClickOutside = clickOutside.directive
 
 defineProps({
     tag: {
@@ -24,4 +27,8 @@ const toggle = () => isOpen.value = !isOpen.value
 const toggleClass = computed(() => isOpen.value === true ? 'show' : '')
 
 const menuStyle = computed(() => isOpen.value === true ? 'position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate(0px, 40px);' : '')
+
+const handleClickOutside = () => {
+    isOpen.value = false
+}
 </script>
