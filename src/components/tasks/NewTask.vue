3c45e25e44d25c07ffdd11fee5469946814cf5fb
Eding Muhamad <masaruedo@yahoo.com>
2023-06-01 18:21:20 +0700

    Add flag icon to each priority item

--- src/components/tasks/NewTask.vue
+++ src/components/tasks/NewTask.vue
@@ -9,11 +9,39 @@
             <div class="dropdown">
                 <button class="btn btn-sm btn-light dropdown-toggle show" type="button" data-bs-toggle="dropdown" aria-expanded="false">                
                 </button>
-                <ul class="dropdown-menu show">
-                    <li><a class="dropdown-item" href="#">High Priority</a></li>
-                    <li><a class="dropdown-item" href="#">Medium Priority</a></li>
-                    <li><a class="dropdown-item" href="#">Low Priority</a></li>
-                    <li><a class="dropdown-item" href="#">No Priority</a></li>
+                <ul class="dropdown-menu show" style="position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate(0px, 40px);">
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <span class="text-danger me-2">
+                                <IconFlag />
+                            </span>
+                            High Priority
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <span class="text-warning me-2">
+                                <IconFlag />
+                            </span>
+                            Medium Priority
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <span class="text-primary me-2">
+                                <IconFlag />
+                            </span>
+                            Low Priority
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <span class="text-secondary me-2">
+                                <IconFlag />
+                            </span>
+                            No Priority
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -23,6 +51,7 @@
 <script setup>
 import { reactive } from 'vue';
 import { useTaskStore } from "../../stores/task";
+import IconFlag from '../icons/IconFlag.vue';
 
 const store = useTaskStore()
 const { handleAddedTask } = store
