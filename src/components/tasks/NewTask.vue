b279b03a3c6ecb4034ac5742f2b4ba84e961d447
Eding Muhamad <masaruedo@yahoo.com>
2023-06-01 18:10:58 +0700

    Add a dropdown priorities

--- src/components/tasks/NewTask.vue
+++ src/components/tasks/NewTask.vue
@@ -5,6 +5,18 @@
             placeholder="+ Add new task. Press enter to save." 
             @keydown.enter="addNewTask"
         />
+        <div class="sort-priority">
+            <div class="dropdown">
+                <button class="btn btn-sm btn-light dropdown-toggle show" type="button" data-bs-toggle="dropdown" aria-expanded="false">                
+                </button>
+                <ul class="dropdown-menu show">
+                    <li><a class="dropdown-item" href="#">High Priority</a></li>
+                    <li><a class="dropdown-item" href="#">Medium Priority</a></li>
+                    <li><a class="dropdown-item" href="#">Low Priority</a></li>
+                    <li><a class="dropdown-item" href="#">No Priority</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -28,3 +40,18 @@ const addNewTask = async(event) => {
     }
 }
 </script>
+
+<style scopded>
+.relative {
+    position: relative;
+}
+
+.sort-priority {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 10px;
+    padding-left: 10px;
+    z-index: 1000;
+}
+</style>
