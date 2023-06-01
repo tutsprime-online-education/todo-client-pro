c709141cc00f171ab7537dbe86cce0b97288c947
Eding Muhamad <masaruedo@yahoo.com>
2023-06-01 17:06:05 +0700

    Add a dropdown markup in the TasksPage component

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -3,6 +3,21 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
+                    <!-- Task order -->
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <h3 class="text-body mb-0">Tasks</h3>
+                        <div class="dropdown">
+                            <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <IconArrowDownUp />
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Sort by time</a></li>
+                                <li><a class="dropdown-item" href="#">Sort by name</a></li>
+                                <li><a class="dropdown-item" href="#">Sort by priority</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
                     <!-- Add new Task -->
                     <NewTask />
 
@@ -31,6 +46,7 @@ import { storeToRefs } from "pinia";
 import { useTaskStore } from "../stores/task";
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
+import IconArrowDownUp from "../components/icons/IconArrowDownUp.vue";
 
 const store = useTaskStore()
 const { completedTasks, uncompletedTasks } = storeToRefs(store)
